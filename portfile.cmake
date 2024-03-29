24b68c9f81d23cd42d8544187c09bd4c7cfea0ff
pfhgil <stukalovila76@gmail.com>
2024-03-29 23:58:13 +0300

    upd

--- portfile.cmake
+++ portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Pixelfield-ru/SungearEngine
         REF "${VERSION}"
-        SHA512 0
+        SHA512 13c7599c157e50d273120312b17a41bc8e62b075146f9bffe566bfc6cb07aded9b6e0e20098787d05806e6fdc884357174fbf272a38018f7e39540bb4e85f300
         HEAD_REF main
 )
 
@@ -12,7 +12,7 @@ vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_cmake_install()
+vcpkg_cmake_install([ADD_BIN_TO_PATH])
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME "sungearengine")
 
