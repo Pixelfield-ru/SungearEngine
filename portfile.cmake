09f1dad68ca33ebc1c413ba804c798b50ca7c26f
pfhgil <stukalovila76@gmail.com>
2024-03-29 21:46:25 +0300

    upd

--- portfile.cmake
+++ portfile.cmake
@@ -1,2 +1,22 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO Pixelfield-ru/SungearEngine
+        REF "${VERSION}"
+        SHA512 0  # This is a temporary value. We will modify this value in the next section.
+        HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "sungearengine")
+
+# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+#[[file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)]]
