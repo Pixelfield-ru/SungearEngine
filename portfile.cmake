e4afd3cb92e43af286e0dd67de5fa15d71cf554e
pfhgil <stukalovila76@gmail.com>
2024-03-30 22:35:51 +0300

    upd

--- portfile.cmake
+++ portfile.cmake
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "sungearengine-sdk")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "sungearengine")
 
 # file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
