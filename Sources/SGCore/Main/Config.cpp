95b8dfa1efd5f8e19c6a97aa6249b14f6657af7f
pfhgil <stukalovila76@gmail.com>
2024-05-05 17:21:40 +0300

    upd

---
+++ Sources/SGCore/Main/Config.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ilya on 05.05.24.
+//
+#include "Config.h"
+
+void SGCore::Config::save(const std::string& path) const noexcept
+{
+
+}
+
+void SGCore::Config::load(const std::string& path) const noexcept
+{
+
+}
+
