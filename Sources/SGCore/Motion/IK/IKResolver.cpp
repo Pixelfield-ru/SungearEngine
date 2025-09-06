1e1edd4e676cf217647b3be35b2400c3156cd312
pfhgil <stukalovila76@gmail.com>
2025-09-07 01:34:28 +0300

    ik upd

--- Sources/SGCore/Motion/IK/IKResolver.cpp
+++ Sources/SGCore/Motion/IK/IKResolver.cpp
@@ -31,10 +31,11 @@ void SGCore::IKResolver::fixedUpdate(const double& dt, const double& fixedDt)
         collectJoints(*registry, entityBaseInfo, jointsChains, *jointsChains.begin());
 
         // processing joints chains
-        // skipping first chain because it contains only root joint
-        for(size_t i = 1; i < jointsChains.size(); ++i)
+        for(size_t i = 0; i < jointsChains.size(); ++i)
         {
             const auto& chain = jointsChains[i];
+
+            std::cout << "chain joints count: " << chain.size() << std::endl;
 
             if(chain.size() < 2) continue;
 
@@ -53,49 +54,97 @@ void SGCore::IKResolver::fixedUpdate(const double& dt, const double& fixedDt)
                 jointsTransforms.push_back(jointTransform);
             }
 
+            float totalChainLength = 0;
+
             // calculating bones lengths
             for(size_t j = 0; j < jointsCount - 1; ++j)
             {
                 const float boneLength = glm::distance(jointsTransforms[j]->m_finalTransform.m_position, jointsTransforms[j + 1]->m_finalTransform.m_position);
                 bonesLengths.push_back(boneLength);
+                totalChainLength += boneLength;
             }
 
             // classic FABRIK algorithm =======================================
 
-            if(!registry->get<IKJoint>(chain[jointsCount - 1]).m_targetPosition) continue;
+            const auto& effectorTransform = jointsTransforms[jointsCount - 1];
+            auto& effectorJoint = registry->get<IKJoint>(chain[jointsCount - 1]);
 
-            const auto targetPos = *registry->get<IKJoint>(chain[jointsCount - 1]).m_targetPosition;
+            if(!effectorJoint.m_targetPosition) continue;
 
-            jointsTransforms[jointsCount - 1]->m_ownTransform.m_position = glm::inverse(jointsTransforms[jointsCount - 1]->m_finalTransform.m_animatedModelMatrix) * glm::vec4(targetPos, 1.0);
+            const auto targetPos = *effectorJoint.m_targetPosition;
+            glm::vec3 targetGlobalPos = jointsTransforms[jointsCount - 2]->m_finalTransform.m_animatedModelMatrix * glm::vec4(*effectorJoint.m_targetPosition, 1.0f);
+
+            // ====================================================
+
+            const auto rootPos = jointsTransforms[0]->m_ownTransform.m_position;
+            const auto rootGlobalPos = jointsTransforms[0]->m_finalTransform.m_position;
+
+            const float distToTarget = glm::distance(rootGlobalPos, targetGlobalPos);
+            if(distToTarget > totalChainLength)
+            {
+                /*glm::vec3 direction = glm::normalize(m_target - root);
+                for (size_t i = 1; i < m_points.size(); i++) {
+                    m_points[i].position = m_points[i-1].position + direction * m_lengths[i-1];
+                }*/
+
+                // if(glm::distance2(jointsTransforms[jointsCount - 1]->m_ownTransform.m_position, targetPos) < tolerance * tolerance)
+                {
+                    effectorJoint.m_targetPosition = std::nullopt;
+                }
+
+                continue;
+            }
+
+            // ====================================================
+
+            // jointsTransforms[jointsCount - 1]->m_ownTransform.m_position = glm::inverse(jointsTransforms[jointsCount - 1]->m_finalTransform.m_animatedModelMatrix) * glm::vec4(targetPos, 1.0);
+            effectorTransform->m_ownTransform.m_position = targetPos;
 
             // backward
             for(std::int64_t j = jointsCount - 2; j >= 0; --j)
             {
-                const float d = glm::distance(jointsTransforms[j + 1]->m_finalTransform.m_position, jointsTransforms[j]->m_finalTransform.m_position);
+                glm::vec3 dir = glm::normalize(jointsTransforms[j]->m_finalTransform.m_position - jointsTransforms[j + 1]->m_finalTransform.m_position);
+                const glm::vec3 globalPos = jointsTransforms[j + 1]->m_finalTransform.m_position + dir * bonesLengths[j];
+
+                glm::vec3 localPos = glm::vec4(globalPos, 0.0);
+                if(j - 1 >= 0)
+                {
+                    localPos = glm::inverse(jointsTransforms[j - 1]->m_finalTransform.m_animatedModelMatrix) * glm::vec4(globalPos, 0.0);
+                }
+
+                jointsTransforms[j]->m_ownTransform.m_position = localPos;
+                // jointsTransforms[j]->m_ownTransform.m_position = globalPos;
+
+                /*const float d = glm::distance(jointsTransforms[j + 1]->m_finalTransform.m_position, jointsTransforms[j]->m_finalTransform.m_position);
                 const float lambda = bonesLengths[j] / d;
 
                 const glm::vec3 globalPosition = (1 - lambda) * jointsTransforms[j + 1]->m_finalTransform.m_position + lambda * jointsTransforms[j]->m_finalTransform.m_position;
                 const glm::vec3 localPosition = glm::inverse(jointsTransforms[j]->m_finalTransform.m_animatedModelMatrix) * glm::vec4(globalPosition, 1.0);
 
-                jointsTransforms[j]->m_ownTransform.m_position = localPosition;
+                jointsTransforms[j]->m_ownTransform.m_position = localPosition;*/
             }
 
             // forward
-            // joints[0] = rootPosition;
-            for(std::int64_t j = 0; j < jointsCount - 1; ++j)
+            jointsTransforms[0]->m_ownTransform.m_position = rootPos;
+
+            for(std::int64_t j = 1; j < jointsCount; ++j)
             {
-                const float d = glm::distance(jointsTransforms[j + 1]->m_finalTransform.m_position, jointsTransforms[j]->m_finalTransform.m_position);
-                float lambda = bonesLengths[j] / d;
+                glm::vec3 dir = glm::normalize(jointsTransforms[j]->m_finalTransform.m_position - jointsTransforms[j - 1]->m_finalTransform.m_position);
+                const glm::vec3 globalPos = jointsTransforms[j - 1]->m_finalTransform.m_position + dir * bonesLengths[j - 1];
 
-                const glm::vec3 globalPosition = (1 - lambda) * jointsTransforms[j]->m_finalTransform.m_position + lambda * jointsTransforms[j + 1]->m_finalTransform.m_position;
-                const glm::vec3 localPosition = glm::inverse(jointsTransforms[j]->m_finalTransform.m_animatedModelMatrix) * glm::vec4(globalPosition, 1.0);
+                glm::vec3 localPos = glm::vec4(globalPos, 0.0);
+                if(j - 1 >= 0)
+                {
+                    localPos = glm::inverse(jointsTransforms[j - 1]->m_finalTransform.m_animatedModelMatrix) * glm::vec4(globalPos, 0.0);
+                }
 
-                jointsTransforms[j + 1]->m_ownTransform.m_position = localPosition;
+                jointsTransforms[j]->m_ownTransform.m_position = localPos;
+               // jointsTransforms[j]->m_ownTransform.m_position = globalPos;
             }
 
-            if(glm::distance2(jointsTransforms[jointsCount - 1]->m_finalTransform.m_position, targetPos) < tolerance * tolerance)
+            if(glm::distance2(effectorTransform->m_ownTransform.m_position, targetPos) < tolerance * tolerance)
             {
-                registry->get<IKJoint>(chain[jointsCount - 1]).m_targetPosition = std::nullopt;
+                effectorJoint.m_targetPosition = std::nullopt;
             }
          }
 
@@ -115,13 +164,25 @@ void SGCore::IKResolver::collectJoints(const ECS::registry_t& inRegistry,
         currentJointsChain.push_back(currentIKEntity);
     }
 
+    bool isMoreThanOneChain = false;
+
     for(auto childEntity : currentIKEntityInfo.getChildren())
     {
-        std::vector<ECS::entity_t> newChain = currentJointsChain;
+        auto newChain = currentJointsChain;
 
         collectJoints(inRegistry, inRegistry.get<EntityBaseInfo>(childEntity), jointsChains, newChain);
 
-        jointsChains.push_back(std::move(newChain));
+        // if new chain has new joints
+        if(newChain.size() != currentJointsChain.size())
+        {
+            if(!isMoreThanOneChain)
+            {
+                currentJointsChain = std::move(newChain);
+                isMoreThanOneChain = true;
+                continue;
+            }
+            jointsChains.push_back(std::move(newChain));
+        }
     }
 }
 
